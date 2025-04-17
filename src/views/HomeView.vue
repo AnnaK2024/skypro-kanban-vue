a5bcb13421f1fb3484b92f3e3926328ceec257d4
annak2024 <kalininaann.pk@mail.ru>
2025-04-17 19:54:15 +0300

    Распределила разметку по компонентам

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,3 @@
-<script setup>
-</script>
 
 <template>
   <main>
@@ -277,10 +275,10 @@
 			<div class="container">
 				<div class="header__block">
 					<div class="header__logo _show _light">
-						<a href="" target="_self"><img src="images/logo.png" alt="logo"></a>
+						<a href="" target="_self"><img src="@/assets/images/logo.png" alt="logo"></a>
 					</div>
 					<div class="header__logo _dark">
-						<a href="" target="_self"><img src="images/logo_dark.png" alt="logo"></a>
+						<a href="" target="_self"><img src="@/assets/images/logo_dark.png" alt="logo"></a>
 					</div>
 					<nav class="header__nav">
 						<button class="header__btn-main-new _hover01" id="btnMainNew"><a href="#popNewCard">Создать новую задачу</a></button>
@@ -741,3 +739,6 @@
     </div>
   </main>
 </template>
+
+<script setup>
+</script>
