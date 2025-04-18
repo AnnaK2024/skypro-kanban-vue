141cab461854004440d2505e9022f21f29d40ec5
annak2024 <kalininaann.pk@mail.ru>
2025-04-18 23:08:03 +0300

    Добавила объект массивов

---
+++ src/components/TaskDesk.vue
@@ -0,0 +1,22 @@
+<script setup>
+import BaseHeader from '@/components/BaseHeader.vue';
+import BaseTask from '@/components/BaseTask.vue';
+import ExitModal from '@/components/ExitModal.vue'
+import NewCardModal from '@/components/NewCardModal.vue';
+import TaskColumn from '@/components/TaskColumn.vue';
+import TaskModal from '@/components/TaskModal.vue';
+
+</script>
+
+<template>
+  <main>
+    <ExitModal />
+    <BaseHeader />
+    <NewCardModal />
+    <BaseTask />
+    <TaskModal />
+    <TaskColumn />
+    
+ 
+  </main>
+</template>
---
+++ src/components/moskc/task.js
@@ -0,0 +1,42 @@
+
+const tasks = [
+    {
+      id: 1,
+      topic: 'Vue.js Basics',
+      title: 'Introduction to Vue.js',
+      date: '2023-10-01',
+      status: 'без статуса'
+    },
+    {
+      id: 2,
+      topic: 'Vue.js Components',
+      title: 'Creating Reusable Components',
+      date: '2023-10-05',
+      status: 'нужно сделать'
+    },
+    {
+      id: 3,
+      topic: 'Vue.js State Management',
+      title: 'Understanding Vuex',
+      date: '2023-10-10',
+      status: 'в работе'
+    },
+    {
+      id: 4,
+      topic: 'Vue.js Routing',
+      title: 'Setting Up Vue Router',
+      date: '2023-10-15',
+      status: 'тестирование'
+    },
+    {
+      id: 5,
+      topic: 'Vue.js Performance',
+      title: 'Optimizing Vue Applications',
+      date: '2023-10-20',
+      status: 'готово'
+    }
+  ];
+  
+  // Экспортируем массив для использования в других файлах
+  export default tasks;
+  
