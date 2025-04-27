0ca2fe1bdb624c5c4d437163843ff9923c45a826
annak2024 <kalininaann.pk@mail.ru>
2025-04-27 14:12:49 +0300

    Готовое ДЗ Props, ref, onMounted с учетом корректировок

--- src/components/TaskColumn.vue
+++ src/components/TaskColumn.vue
@@ -1,42 +1,23 @@
 <template>
-  <tasks />
-
-  <div>
-    <main class="main">
-      <div class="container">
-        <div v-if="loading" class="loading">Данные загружаются...</div>
-        <div v-else>
-          <div class="main__block">
-            <div v-for="status in statuses" :key="status">
-              <div class="column__title">
-                <p>{{ status }}</p>
-              </div>
-              <div class="cards">
-                <div v-for="card in filteredTasks(status)" :key="card.id">
-                  <TaskCard :card="card" />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+  <div class="main__column">
+    <div class="column__title">
+      <p>{{ status }}</p>
+    </div>
+    <div class="cards">
+      <div v-for="card in tasks" :key="card.id">
+        <TaskCard :card="card" />
       </div>
-    </main>
+    </div>
   </div>
 </template>
 
 <script setup>
-import { tasks } from './mocks/tasks'
 import TaskCard from './TaskCard.vue'
 
 defineProps({
-  loading: Boolean,
+  status: { type: String, required: true },
+  tasks: { type: Array, required: true },
 })
-
-const statuses = ['Без статуса', 'Нужно сделать', 'В работе', 'Тестирование', 'Готово']
-
-const filteredTasks = (status) => {
-  return tasks.filter((task) => task.status === status)
-}
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped></style>
--- src/components/TaskDesk.vue
+++ src/components/TaskDesk.vue
@@ -1,3 +1,29 @@
+<template>
+  <main>
+    <ExitModal />
+    <BaseHeader />
+    <NewCardModal />
+    <BaseTask />
+    <div class="main">
+      <div class="container">
+        <div class="main__block">
+          <div class="main__content">
+            <div v-if="loading" class="loading">Данные загружаются...</div>
+            <div v-else class="columns">
+              <TaskColumn
+                v-for="status in statuses"
+                :key="status"
+                :status="status"
+                :tasks="filteredTasks(status)"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
 <script setup>
 import BaseHeader from '@/components/BaseHeader.vue'
 import BaseTask from '@/components/BaseTask.vue'
@@ -5,22 +31,30 @@ import ExitModal from '@/components/ExitModal.vue'
 import NewCardModal from '@/components/NewCardModal.vue'
 import TaskColumn from '@/components/TaskColumn.vue'
 import { onMounted, ref } from 'vue'
+import { tasks } from './mocks/tasks'
 
 const loading = ref(true)
+const statuses = ['Без статуса', 'Нужно сделать', 'В работе', 'Тестирование', 'Готово']
+
+const filteredTasks = (status) => {
+  return tasks.filter((task) => task.status === status)
+}
 
 onMounted(() => {
   setTimeout(() => {
     loading.value = false
-  }, 3000)
+  }, 1000)
 })
 </script>
 
-<template>
-  <main>
-    <ExitModal />
-    <BaseHeader />
-    <NewCardModal />
-    <BaseTask />
-    <TaskColumn :loading="loading" />
-  </main>
-</template>
+<style scoped>
+.loading {
+  text-align: center;
+  font-size: 1.5em;
+  margin: 20px;
+}
+
+.columns {
+  display: flex;
+}
+</style>
